3eb500f30d8d781e08a733d3f3c3ad539a9ef47e
MCBHammer <armanaznavour@yahoo.com>
2021-09-15 03:10:38 -0500

    Game Controller for Reset and Quit added

---
+++ Assets/Scripts/Common/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private int _sceneIndex;
+    private void Awake()
+    {
+        _sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            //reload currently active scene
+            SceneManager.LoadScene(_sceneIndex);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //quit application
+            Debug.Log("Quit!");
+            Application.Quit();
+        }
+    }
+}
