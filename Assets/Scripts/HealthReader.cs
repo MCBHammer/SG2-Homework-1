1874a2ef22e34666d16d0602d458f226f5493406
MCBHammer <armanaznavour@yahoo.com>
2021-09-27 01:54:50 -0500

    Healthbar v0.1

---
+++ Assets/Scripts/HealthReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthReader : MonoBehaviour
+{
+    float _healthValue;
+    Health _healthDetected;
+    Slider _healthBar;
+
+    private void Awake()
+    {
+        _healthBar = GetComponent<Slider>();
+    }
+    private void Start()
+    {
+        //_healthDetected.Health
+        _healthBar.maxValue = _healthValue;
+        _healthBar.value = _healthValue;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _healthBar.value = _healthValue;
+    }
+}
