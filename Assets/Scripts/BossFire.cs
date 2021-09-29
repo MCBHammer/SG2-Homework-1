01a0dbca34ac8f57cefee4f13b14e0e62bacd9c9
MCBHammer <armanaznavour@yahoo.com>
2021-09-29 00:29:47 -0500

    Attack 2 and Attack 1 charge animation

--- Assets/Scripts/BossFire.cs
+++ Assets/Scripts/BossFire.cs
@@ -11,14 +11,31 @@ public class BossFire : FireController
 
     [SerializeField] GameObject _specialProj;
     [SerializeField] float _specialProjChance = 0.3f;
+    [SerializeField] float _chargeTime = 0.5f;
+
+    float _prob;
+    Boss _boss;
 
     // Start is called before the first frame update
     private void Awake()
     {
         _tr = this.gameObject.transform;
+        _boss = FindObjectOfType<Boss>();
         float _waitTime = Random.Range(_lowCooldown, _highCooldown);
         StartCoroutine("BFire", _waitTime);
         //Debug.Log(_tr);
+    }
+
+    private void OnEnable()
+    {
+        _boss.Attack2Start += LockFire;
+        _boss.Attack2Stop += UnlockFire;
+    }
+
+    private void OnDisable()
+    {
+        _boss.Attack2Start -= LockFire;
+        _boss.Attack2Stop -= UnlockFire;
     }
 
     private void Update()
@@ -29,7 +46,6 @@ public class BossFire : FireController
     public override void Fire()
     {
         FireFeedback();
-        float _prob = Random.value;
         //Debug.Log(_prob);
         if (_prob >= _specialProjChance)
         {
@@ -52,8 +68,45 @@ public class BossFire : FireController
 
     private IEnumerator BFire(float WaitTime)
     {
-        yield return new WaitForSeconds(WaitTime);
+        _prob = Random.value;
+        yield return new WaitForSeconds(WaitTime - _chargeTime);
+
+        GameObject _BossTurret = this.transform.Find("Art/Turret").gameObject;
+        Color _BossMaterial = _BossTurret.GetComponent<Renderer>().material.color;
+        Color _projMaterial = _projectile.transform.Find("Art/Sphere").gameObject.GetComponent<Renderer>().sharedMaterial.color;
+        Color _specialProjMaterial = _specialProj.transform.Find("Art/Sphere").gameObject.GetComponent<Renderer>().sharedMaterial.color;
+        Color _currentColor;
+
+        float currentTime = 0;
+        while (currentTime < _chargeTime)
+        {
+            currentTime += Time.deltaTime;
+            float t = currentTime / _chargeTime;
+            if(_prob >= _specialProjChance)
+            {
+                _currentColor = Color.Lerp(_BossMaterial, _projMaterial, t);
+            } else
+            {
+                _currentColor = Color.Lerp(_BossMaterial, _specialProjMaterial, t);
+            }
+            _BossTurret.GetComponent<Renderer>().material.color = _currentColor;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(_chargeTime);
         Fire();
+        _BossTurret.GetComponent<Renderer>().material.color = _BossMaterial;
+        float _waitTime = Random.Range(_lowCooldown, _highCooldown);
+        StartCoroutine("BFire", _waitTime);
+    }
+
+    private void LockFire()
+    {
+        StopCoroutine("BFire");
+    }
+
+    private void UnlockFire()
+    {
         float _waitTime = Random.Range(_lowCooldown, _highCooldown);
         StartCoroutine("BFire", _waitTime);
     }
--- Assets/Scripts/Enemies/Boss.cs
+++ Assets/Scripts/Enemies/Boss.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System;
 
 public class Boss : MonoBehaviour
 {
     [SerializeField] float _moveSpeed = .25f;
     [SerializeField] float _wanderRadius = 8f;
+    [SerializeField] float _detectDistance = 5;
     [SerializeField] AudioClip _trampleSound;
+    [SerializeField] AudioClip _attack2Sound;
+
+    [SerializeField] float _attack2Time = 2;
+    [SerializeField] float _runAwayTime = 4;
+    [SerializeField] float _attackRadius = 8f;
+    bool _isAttack2 = false;
+
+    public event Action Attack2Start;
+    public event Action Attack2Stop;
+
+    public bool Attack2Bool
+    {
+        get => _isAttack2;
+        set {; }
+    }
 
     NavMeshAgent agent;
+
+    Player _player;
     public float MoveSpeed
     {
         get => _moveSpeed;
@@ -30,6 +49,7 @@ public class Boss : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _player = FindObjectOfType<Player>();
 
         //When working on randomization, reference https://answers.unity.com/questions/475066/how-to-get-a-random-point-on-navmesh.html for help
         agent = GetComponent<NavMeshAgent>();
@@ -38,6 +58,18 @@ public class Boss : MonoBehaviour
         */
 
         resetTarget();
+    }
+
+    private void OnEnable()
+    {
+        Attack2Start += StopMoving;
+        Attack2Stop += StartMoving;
+    }
+
+    private void OnDisable()
+    {
+        Attack2Start -= StopMoving;
+        Attack2Stop -= StartMoving;
     }
 
 
@@ -49,16 +81,29 @@ public class Boss : MonoBehaviour
         float _distance = agent.remainingDistance;
         if(_distance != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && _distance <= 5.5)
         {
-            //check if close enough to target
-            resetTarget();
+            if(_isAttack2 == false)
+            {
+                //check if close enough to target
+                resetTarget();
+                //Debug.Log("It was me!");
+            } 
         }
         if(agent.pathStatus == NavMeshPathStatus.PathPartial || agent.pathStatus == NavMeshPathStatus.PathInvalid ||_distance == Mathf.Infinity || _distance <= 8)
         {
-            //If target is too close or invalid position
-            resetTarget();
-            Debug.Log("Whoops");
+            if(_isAttack2 == false)
+            {
+                //If target is too close or invalid position
+                resetTarget();
+                //Debug.Log("Whoops");
+            }
         }
 
+        //Keeping the if statement compact
+        float _playerDistance = (_player.transform.position - this.transform.position).sqrMagnitude;
+        if (_playerDistance < _detectDistance * _detectDistance  && _isAttack2 == false)
+        {
+            StartCoroutine(Attack2(_attack2Time, _runAwayTime));
+        }
         //Debug.Log(agent.destination);
         //Debug.Log(agent.remainingDistance);
     }
@@ -105,12 +150,83 @@ public class Boss : MonoBehaviour
 
     private void resetTarget()
     {
-        Vector3 _randomDirection = Random.insideUnitSphere * _wanderRadius;
+        Vector3 _randomDirection = UnityEngine.Random.insideUnitSphere * _wanderRadius;
         _randomDirection += agent.transform.position;
         NavMeshHit _hit;
         NavMesh.SamplePosition(_randomDirection, out _hit, _wanderRadius, 1);
         Vector3 _finalPosition = _hit.position;
         agent.destination = _finalPosition;
-        Debug.Log(_finalPosition);
+        //Debug.Log(_finalPosition);
+    }
+
+    private IEnumerator Attack2(float _attackTime, float _runTime)
+    {
+        _isAttack2 = true;
+        Attack2Start?.Invoke();
+
+        //Texture Change
+        float currentTime = 0;
+        GameObject _BossBody = this.transform.Find("Art/Body").gameObject;
+        GameObject _BossTurret = this.transform.Find("Art/Turret").gameObject;
+        Color _BossMaterial = _BossBody.GetComponent<Renderer>().material.color;
+        while(currentTime < _attackTime)
+        {
+            currentTime += Time.deltaTime;
+            float t = currentTime / _attackTime;
+            Color _currentColor = Color.Lerp(_BossMaterial, Color.white, t);
+            _BossBody.GetComponent<Renderer>().material.color = _currentColor;
+            _BossTurret.GetComponent<Renderer>().material.color = _currentColor;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(_attackTime);
+
+        AttackNearby();
+        _BossBody.GetComponent<Renderer>().material.color = _BossMaterial;
+        _BossTurret.GetComponent<Renderer>().material.color = _BossMaterial;
+        agent.isStopped = false;
+        RunAway();
+
+        yield return new WaitForSeconds(_runTime);
+        _isAttack2 = false;
+        Attack2Stop?.Invoke();
+    }
+
+    private void AttackNearby()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, _attackRadius);
+        for(int i  = 0; i < hitColliders.Length; i++)
+        {
+            IDamageable _hit = hitColliders[i].GetComponent<IDamageable>();
+            Boss _boss = hitColliders[i].GetComponent<Boss>();
+            if(_hit != null && _boss == null)
+            {
+                _hit.TakeDamage(2);
+            }
+        }
+        if (_attack2Sound != null)
+        {
+            AudioHelper.PlayClip2D(_attack2Sound, .75f);
+        }
+    }
+
+    private void RunAway()
+    {
+        Vector3 _towardsPlayer = _player.transform.position - this.transform.position;
+        NavMeshHit _hit;
+        NavMesh.SamplePosition(-_towardsPlayer, out _hit, 8, 1);
+        Vector3 _finalPosition = _hit.position;
+        agent.destination = _finalPosition;
+    }
+
+    private void StopMoving()
+    {
+        agent.isStopped = true;
+        //Debug.Log("This is working");
+    }
+
+    private void StartMoving()
+    {
+        resetTarget();
     }
 }
